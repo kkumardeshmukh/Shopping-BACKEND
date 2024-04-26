90190e53cda047f0341b9a62667d4b7a53d0aea3
kkumardeshmukh <kdkrantikumar@gmail.com>
2024-04-26 17:03:14 +0530

    category-CRUD operation Done

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -2,11 +2,8 @@ import categoryModel from "../models/categoryModel.js"
 import slugify from 'slugify'
 
 export const createCategoryController = async (req, res) => {
-
     try {
-
         const { name } = req.body
-
         if (!name) {
             return res.status(400).send({
                 success: false,
@@ -14,37 +11,110 @@ export const createCategoryController = async (req, res) => {
             }
             )
         }
-
         const existingCategory = await categoryModel.findOne({ name })
         if (existingCategory) {
-
             return res.status(500).send({
                 success: false,
                 message: "Entered category already exist"
             }
             )
         }
-
         const category = await new categoryModel({ name, slug: slugify(name) }).save()
-
         res.status(201).send({
             success: true,
             message: "Category Created Successfully",
             category
         }
         )
-
     }
     catch (error) {
         console.log(error)
         res.status(500).send({
-
             success: false,
             message: "error in creating categiry"
         }
         )
-
+    }
+}
+export const updateCategoryController = async (req, res) => {
+    try {
+        const { name } = req.body
+        const { id } = req.params
+        if (!name) {
+            return res.status(500).send({
+                success: false,
+                message: "provide category name to update"
+            }
+            )
+        }
+        const category = await categoryModel.findByIdAndUpdate(id, { name, slug: slugify(name) }, { new: true })
+        res.status(200).send({
+            success: true,
+            message: "category updated successfully"
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            message: "error while updating category"
+        })
     }
 }
 
-export const updateCategoryController = () => { }
+export const getCategoryController = async (req, res) => {
+
+    try {
+        const allCategories = await categoryModel.find({})
+
+        res.status(200).send({
+            success: true,
+            message: "all categories are following",
+            allCategories
+        }
+        )
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success: flase,
+            message: "error while fetching all categories"
+        })
+    }
+}
+
+export const singleCategoryController = async (req, res) => {
+
+    try {
+        const cate = await categoryModel.findOne({ slug: req.params.slug })
+        res.status(200).send({
+            success: true,
+            message: "single category",
+            cate
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            message: "error while getting single category"
+        })
+    }
+}
+
+export const deleteCategoryController = async (req, res) => {
+
+    try {
+        const { id } = req.params
+        await categoryModel.findByIdAndDelete()
+        res.status(200).send({
+            success: false,
+            message: "Delete category successfully"
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            message: "error while deletting category"
+        })
+
+    }
+
+}
--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,14 +1,23 @@
 import express from "express";
 import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
-import { createCategoryController, updateCategoryController } from "../controller/categoryController.js";
+import { createCategoryController, deleteCategoryController, getCategoryController, singleCategoryController, updateCategoryController } from "../controller/categoryController.js";
 
 //router configuration
 const router = express.Router()
 
-//crating category
+//creating category
 router.post('/create-category', requireSignIn, isAdmin, createCategoryController)
 
 //updating categories
-router.put('/update-category', requireSignIn, isAdmin, updateCategoryController)
+router.put('/update-category/:id', requireSignIn, isAdmin, updateCategoryController)
+
+//get all catagories
+router.get('/get-category', getCategoryController)
+
+//get single category by its slug
+router.get('/single-category/:slug', singleCategoryController)
+
+//delete category
+router.delete('/delete-category/:id', requireSignIn, isAdmin, deleteCategoryController)
 
 export default router
